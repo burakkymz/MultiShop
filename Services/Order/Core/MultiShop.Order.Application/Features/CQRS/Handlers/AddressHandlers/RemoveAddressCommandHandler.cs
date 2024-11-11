a79a1b2ae52953b9646f30980a6c5ca1d726fd09
Burak Kaymaz <burakkymz@gmail.com>
2024-11-11 18:22:10 +0300

    Add address management commands and handlers
    Introduced new command and handler classes for managing addresses in the `MultiShop.Order.Application` project. Added `CreateAddressCommand`, `RemoveAddressCommand`, `UpdateAddressCommand`, `CreateAddressCommandHandler`, `GetAddressByIdQueryHandler`, `GetAddressQueryHandler`, `RemoveAddressCommandHandler`, and `UpdateAddressCommandHandler`. These classes handle CRUD operations on `Address` entities. Updated `MultiShop.Order.Application.csproj` to reference `MultiShop.Order.Domain` and adjusted folder structure in the project file.

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/AddressHandlers/RemoveAddressCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MultiShop.Order.Application.Features.CQRS.Commands.AddressCommands;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers
+{
+    public class RemoveAddressCommandHandler
+    {
+        private readonly IRepository<Address> _repository;
+
+        public RemoveAddressCommandHandler(IRepository<Address> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(RemoveAddressCommand addressCommand)
+        {
+            var values = await _repository.GetByIdAsync(addressCommand.Id);
+            await _repository.DeleteAsync(values);
+        }
+    }
+}
