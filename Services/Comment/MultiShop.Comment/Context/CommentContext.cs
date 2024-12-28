4b5e8e8b5d1e32985337214037d4dd39618c7562
burak kaymaz <burakkaymaz0101@gmail.com>
2024-12-28 18:47:13 +0300

    Add MultiShop.Comment project with initial setup
    Added a new "MultiShop.Comment" project to the solution, including
    configuration files, project references, and initial setup. Updated
    `MultiShop.sln` to include the new project and nested it under a new
    "Comment" solution folder. Created `Program.cs` to set up the ASP.NET
    Core application with necessary services and Swagger. Defined
    `CommentContext` and `UserComment` entity for Entity Framework Core.
    Added initial migration files and `launchSettings.json` for project
    configuration.

---
+++ Services/Comment/MultiShop.Comment/Context/CommentContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiShop.Comment.Entities;
+
+namespace MultiShop.Comment.Context
+{
+    public class CommentContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer
+                ("Server=localhost,1442;Initial Catalog=MultiShopCommentDb;User Id=sa;Password=Burak.01;TrustServerCertificate=True;");
+        }
+
+        public DbSet<UserComment> UserComments { get; set; }
+    }
+}
